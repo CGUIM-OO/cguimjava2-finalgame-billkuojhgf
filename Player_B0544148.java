4c8b68b40d23c4687af9d21763bfce42e3945f57
billkuojhgf <33015884+billkuojhgf@users.noreply.github.com>
2018-01-09 01:57:23 +0800

    bet increase to 142

--- Player_B0544148.java
+++ Player_B0544148.java
@@ -2,7 +2,7 @@
 public class Player_B0544148 extends Player {
 	
 	private int mybet_prev = 1000;//先前的bet
-	private int mybet_made = 80;
+	private int mybet_made = 142;
 	
 
 	public Player_B0544148(int chips){
@@ -14,7 +14,7 @@ public class Player_B0544148 extends Player {
 	@Override
 	public int make_bet() { //馬丁格爾(勝利重返，失敗向前)
 		if(mybet_prev < get_current_chips()){
-			mybet_made = 80;
+			mybet_made = 142;
 		}if(mybet_prev > get_current_chips()){
 			mybet_made = mybet_made * 2;
 		}
