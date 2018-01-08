5f11ae3323f023ef78b9ce097398b67836752428
billkuojhgf <33015884+billkuojhgf@users.noreply.github.com>
2018-01-09 01:46:02 +0800

    BrandNew

--- Player_B0544148.java
+++ Player_B0544148.java
@@ -3,7 +3,6 @@ public class Player_B0544148 extends Player {
 	
 	private int mybet_prev = 1000;//先前的bet
 	private int mybet_made = 80;
-	private int DealerUpCard = 0;
 	
 
 	public Player_B0544148(int chips){
@@ -25,62 +24,13 @@ public class Player_B0544148 extends Player {
 
 	@Override
 	public boolean hit_me(Table table) {
-		int Ace_Count = 0;
-		int OneRoundRank = 0;
-		
-		Card deal = table.get_face_up_card_of_dealer();
-		DealerUpCard = deal.getRank();
-		
-		for(Card c : getOneRoundCard()){
-			if (Ace_Count == 0 && c.getRank() == 1) {
-				Ace_Count = 1;
-				continue;
-			} else {
-				if (c.getRank() == 11 || c.getRank() == 12 || c.getRank() == 13)
-					OneRoundRank += 10;
-				else
-					OneRoundRank += c.getRank();
-			}
-		}
-		if (Ace_Count != 0) {
-			if (OneRoundRank < 11) {
-				OneRoundRank += 11;
-			} else {
-				OneRoundRank += 1;
-			}
+		int total_value = getTotalValue();
 
-		}
-		
-		if(OneRoundRank == 12){
-			if(DealerUpCard >= 4 && DealerUpCard <= 6){
-				return false;
-			}else{
-				return true;
-			}
-		}
-		
-		if(OneRoundRank >= 13 && OneRoundRank <= 16){
-			if(DealerUpCard >= 2 && DealerUpCard <= 6){
-				return false;
-			}else{
-				return true;
-			}
-		}
-		
-		if(Ace_Count >= 1 && OneRoundRank == 18){
-			if(DealerUpCard >= 2 && DealerUpCard <= 8){
-				return false;
-			}else{
-				return true;
-			}
-		}
-			
-		if(OneRoundRank >=17){
+		if (total_value >= 17) {
 			return false;
+		} else {
+			return true;
 		}
-		
-		return true;
-
 	}
 
 }
